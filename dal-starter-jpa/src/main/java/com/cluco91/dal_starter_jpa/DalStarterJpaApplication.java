e5eb218dbb1592f6d226169b9f14cb92a7d42dc8
Cristian Luco <c.luco91@gmail.com>
2025-08-21 15:20:14 -0400

    Added project

---
+++ dal-starter-jpa/src/main/java/com/cluco91/dal_starter_jpa/DalStarterJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cluco91.dal_starter_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalStarterJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalStarterJpaApplication.class, args);
+	}
+
+}
---
+++ dal-starter-jpa/src/test/java/com/cluco91/dal_starter_jpa/DalStarterJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluco91.dal_starter_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalStarterJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
